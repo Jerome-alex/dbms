72a4a2a02195d0c3b4eadb6a48522c70c15f5f28
Jerome-alex <137639468+Jerome-alex@users.noreply.github.com>
2025-03-11 11:52:48 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -3,3 +3,4 @@ create table Department(Dep_id int,dname varchar(20),Location_id int);
 create table Job(Job_id int,funct varchar(30));
 create table Employee(Emp_id int,lname varchar(15),fname varchar(15),mname varchar(15),job_id int,mng_id int,hireda varchar(10),salary int,dep_id int);
 create table Loan(Emp_id int,fname varchar(15),loan_amt int);
+insert into Location(Location_id,reg_grp)values(1,'America'),(2,'Europe'),(3,'Africa'),(4,'China'),(5,'India'),(6,'New_York');
