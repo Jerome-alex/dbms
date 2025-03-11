45f4b765885336ae19c41f4974a451c2bab319e4
Jerome-alex <137639468+Jerome-alex@users.noreply.github.com>
2025-03-11 11:33:27 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -2,4 +2,4 @@ create table Location(Location_id int,reg_grp varchar(20));
 create table Department(Dep_id int,dname varchar(20),Location_id int);
 create table Job(Job_id int,funct varchar(30));
 create table Employee(Emp_id int,lname varchar(15),fname varchar(15),mname varchar(15),job_id int,mng_id int,hireda varchar(10),salary int,dep_id int);
-create table Head(Emp_id int,fname varchar(15),loan_amt int);
+create table Loan(Emp_id int,fname varchar(15),loan_amt int);
